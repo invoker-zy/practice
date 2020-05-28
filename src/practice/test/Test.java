b525c53c7025e45793abae2276674c85d83a25ab
zzy <zzy@DESKTOP-BA4SPRL>
2020-05-28 17:22:33 +0800

    1

--- src/practice/test/Test.java
+++ src/practice/test/Test.java
@@ -1,9 +1,8 @@
 package practice.test;
-
 public class Test{
 	public static void main(String[] args) {
-		int a[ ][ ]={{1,2},{3,4},{5,6}};
-		System.out.println(a[1][1]);
+		int a[]= {1,2,3,4,5};
+		System.out.println();
 	}
 
 }
